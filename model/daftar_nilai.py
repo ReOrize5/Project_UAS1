e7eb73b45bbc67ef7c870a0fac0206f0a060f348
ReOrize5 <115756982+ReOrize5@users.noreply.github.com>
2023-01-09 17:28:58 +0700

    Update daftar_nilai.py

--- model/daftar_nilai.py
+++ model/daftar_nilai.py
@@ -1,16 +1,17 @@
-kontak = {}
+Data_Mahasiswa = {}
 
 
-def tambah_kontak(nama,nim,tugas,uts,uas):
+def tambah_Data_Mahasiswa(nama,nim,tugas,uts,uas):
     akhir = round((float(tugas) * 0.3) + (float(uts) * 0.35) + (float(uas) * 0.35), 2)
-    kontak[nama] = nama,nim,tugas,uts,uas,akhir
+    Data_Mahasiswa[nama] = nama,nim,tugas,uts,uas,akhir
 
 
-def ubah_kontak(nama):
-    if nama in kontak.keys():
-        del kontak[nama]
+def ubah_Data_Mahaiswa(nama):
+    if nama in Data_Mahasiswa.keys():
+        del Data_Mahasiswa[nama]
         print("\n===Masukan Perubahan Data===")
-        
+        from view.input_nilai import inputan
+        inputan()
     else:
         print(" ________________________")
         print("| Data {} tidak ditemukan |".format(nama))
@@ -19,11 +20,11 @@ def ubah_kontak(nama):
 
 
 def cari(nama):
-    if nama in kontak.keys():
+    if nama in Data_Mahasiswa.keys():
         print("\n=====================================================================")
         print("|      NAMA       |       NIM        | TUGAS |  UTS  |  UAS  | AKHIR |")
         print("|=================|==================|=======|=======|===============|")
-        print("| {0:15} | {1:16} | {2:5} | {3:5} | {4:5} | {5:5} |".format(nama, kontak[nama][1], kontak[nama][2],kontak[nama][3],kontak[nama][4], kontak[nama][5]))
+        print("| {0:15} | {1:16} | {2:5} | {3:5} | {4:5} | {5:5} |".format(nama, Data_Mahasiswa[nama][1], Data_Mahasiswa[nama][2],Data_Mahasiswa[nama][3],Data_Mahasiswa[nama][4], Data_Mahasiswa[nama][5]))
         print("======================================================================")
     else:
         print(" ________________________")
@@ -32,8 +33,8 @@ def cari(nama):
 
 
 def hapus(nama):
-    if nama in kontak.keys():
-        del kontak[nama]
+    if nama in Data_Mahasiswa.keys():
+        del Data_Mahasiswa[nama]
         return True
     else:
         print(" ________________________")
